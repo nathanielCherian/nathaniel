33618d22aa0865c5d74804a3725d9c8a86ae6792
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-10 18:23:55 -0700

    merge

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,13 +4,8 @@ import Navbar from '../components/Navbar';
 
 const Index = () => {
   return (
-<<<<<<< HEAD
-    <div className="background">
-      <h1>Nathaniel Cherian Blog</h1>
-=======
     <div className={IndexStyles.container}>
       <Navbar/>
->>>>>>> dff9cd70353c1ac6617c569ed402624ae971419f
     </div>
   )
 }
