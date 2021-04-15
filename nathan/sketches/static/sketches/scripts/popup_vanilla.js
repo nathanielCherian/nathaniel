cdf00a80aeab39a11807b785e891c5c859622c19
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-15 10:44:51 -0700

    move to external script

---
+++ nathan/sketches/static/sketches/scripts/popup_vanilla.js
@@ -0,0 +1,22 @@
+
+    window.addEventListener('load', (event) => {
+    
+        document.getElementById('question').onclick = function changeContent() {
+            document.getElementById("popup").style.display = "block";
+        }
+
+        document.getElementById('popup').onclick = function changeContent() {
+            document.getElementById("popup").style.display = "none";
+        }
+
+        document.getElementById('close-button').onclick = function changeContent() {
+            document.getElementById("popup").style.display = "none";
+        }
+
+        document.addEventListener('keyup', (e) => {
+            if (e.keyCode == 27){
+                document.getElementById("popup").style.display = "none";
+            }
+        });
+
+    });
