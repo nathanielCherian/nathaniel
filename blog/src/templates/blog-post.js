5523824e134dde9ab83f3693717849e645065f7d
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-15 11:30:58 -0700

    responsive

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -11,24 +11,24 @@ const Template = ({data}) => {
 
     return (
         <PageShell>
-            <SEO title= {post.frontmatter.title} description={post.frontmatter.summary} image={post.frontmatter.image} twitterImage={post.frontmatter.twitterImage} ogImage={post.frontmatter.ogImage}/>
+                <SEO title= {post.frontmatter.title} description={post.frontmatter.summary} image={post.frontmatter.image} twitterImage={post.frontmatter.twitterImage} ogImage={post.frontmatter.ogImage}/>
 
-            <div className={BlogPostStyles.article_container__wrapper} >
-                <article className={BlogPostStyles.article_container}>
+                <div className={BlogPostStyles.article_container__wrapper} >
+                    <article className={BlogPostStyles.article_container}>
 
-                    <header>
-                        <h1 className={BlogPostStyles.title}>{post.frontmatter.title}</h1>
-                    </header>
+                        <header>
+                            <h1 className={BlogPostStyles.title}>{post.frontmatter.title}</h1>
+                        </header>
 
-                    <div className={BlogPostStyles.text_section__wrapper}>
-                        <section className={BlogPostStyles.text_section}>
-                            <p className={BlogPostStyles.attribution}> By <em>Nathaniel Cherian</em> | <em>{post.frontmatter.date}</em></p>
-                            <div className={BlogPostStyles.content} dangerouslySetInnerHTML={{__html: post.html}}/>
-                        </section>
-                    </div>
-                    
-                </article>
-            </div>
+                        <div className={BlogPostStyles.text_section__wrapper}>
+                            <section className={BlogPostStyles.text_section}>
+                                <p className={BlogPostStyles.attribution}> By <em>Nathaniel Cherian</em> | <em>{post.frontmatter.date}</em></p>
+                                <div className={BlogPostStyles.content} dangerouslySetInnerHTML={{__html: post.html}}/>
+                            </section>
+                        </div>
+                        
+                    </article>
+                </div>
 
         </PageShell>
     )
