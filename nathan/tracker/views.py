204ddfe9f8c10050afb4aa4199c527b05ce437d7
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-15 22:38:12 -0700

    fix

--- nathan/tracker/views.py
+++ nathan/tracker/views.py
@@ -4,6 +4,5 @@ from PIL import Image
 
 
 def index(request):
-    image_data = open(r"Z:\Computer Science\nathaniel\nathan\home\static\home\portfolio\dontwait.png", "rb").read()
     image = Image.new('RGB', (1,1))
     return HttpResponse(image.getdata(), content_type="image/png")
