8f2b6c21c62e1bc1913b466776203baa626e010d
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-15 22:45:22 -0700

    remove t

--- nathan/tracker/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from PIL import Image
-
-
-def index(request):
-    image = Image.new('RGB', (1,1))
-    return HttpResponse(image.getdata(), content_type="image/png")
