96358d88052d87988ab79e4040df96ac9369bb9e
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-05-02 15:30:05 -0700

    fix portfolio + sketches

--- nathan/sketches/static/sketches/scripts/popup_vanilla.js
+++ nathan/sketches/static/sketches/scripts/popup_vanilla.js
@@ -1,22 +1,22 @@
 
-    window.addEventListener('load', (event) => {
-    
-        document.getElementById('question').onclick = function changeContent() {
-            document.getElementById("popup").style.display = "block";
-        }
+window.addEventListener('load', (event) => {
 
-        document.getElementById('popup').onclick = function changeContent() {
+    document.getElementById('question').onclick = function changeContent() {
+        document.getElementById("popup").style.display = "block";
+    }
+
+    document.getElementById('popup').onclick = function changeContent() {
+        document.getElementById("popup").style.display = "none";
+    }
+
+    document.getElementById('close-button').onclick = function changeContent() {
+        document.getElementById("popup").style.display = "none";
+    }
+
+    document.addEventListener('keyup', (e) => {
+        if (e.keyCode == 27){
             document.getElementById("popup").style.display = "none";
         }
-
-        document.getElementById('close-button').onclick = function changeContent() {
-            document.getElementById("popup").style.display = "none";
-        }
-
-        document.addEventListener('keyup', (e) => {
-            if (e.keyCode == 27){
-                document.getElementById("popup").style.display = "none";
-            }
-        });
-
     });
+
+});
