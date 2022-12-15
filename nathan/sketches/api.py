6491021a022dfb1ec733fbd1d99aa3a66dc982fb
Nathaniel Cherian <ngcherian@gmail.com>
2022-12-15 00:47:31 -0500

    adding chaining to music game

--- nathan/sketches/api.py
+++ nathan/sketches/api.py
@@ -1,8 +1,6 @@
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import requests as r
 
-@csrf_exempt
 def musicmap(request, name):
     res = r.get(f"https://www.music-map.com/{name}")
     return HttpResponse(res.text)
